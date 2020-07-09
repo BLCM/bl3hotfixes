4a4b9dfb4c80e8995521c1e8734f7d2a071f24a1
CJ Kucera <pez@apocalyptech.com>
2020-07-09 11:10:06 -0500

    Auto-update with new hotfixes - 2020-07-09 16:10:03

--- script/csv_investigate.py
+++ script/csv_investigate.py
@@ -51,11 +51,28 @@ with open('hotfixes_combined.csv', 'rt') as df:
                         row['package'],
                         ))
 
-            # Looking into type 6 data
+            # Finding Default__*:Foo hotfixes which use object-redirection in the attr field
+            if True:
+                if 'Default__' in row['object'] and ':' in row['object'] and '..' in row['attr']:
+                    print('{} - {} - {} - {}'.format(
+                        row['object'],
+                        row['attr'],
+                        row['from'],
+                        row['to'],
+                        ))
+
+            # Finding hotfixes which have more than one `Default__` (which, I think, don't actually exist)
+            # (this actually morphed to just look for `Default` in the second part, 'cause I was right...
+            if False:
+                parts = row['object'].split('Default__')
+                if len(parts) > 1:
+                    if 'Default' in parts[-1]:
+                        print(row['object'])
 
         elif row['type'] == '6':
 
-            if True:
+            # Looking into type 6 data
+            if False:
                 print(row['from'])
                 data = row['from'][1:-1]
                 for idx, triple in enumerate(data.split('|')):
